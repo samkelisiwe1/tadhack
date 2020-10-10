3fd7cbc5f0eaba692c9decad1dceea48b5f2a1d7
hardy <hardylutula@gmail.com>
2020-10-10 13:27:21 +0200

    ../implemented imployement cards and landing page

--- src/app/components/job/job.component.ts
+++ src/app/components/job/job.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Job } from 'src/app/models/job.model';
 
 @Component({
   selector: 'Job',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobComponent implements OnInit {
 
+  @Input('job') job: Job;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
